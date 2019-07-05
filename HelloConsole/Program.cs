9953107565eb35871faebd24b783e852d14352ff
Torben Knerr <mail@tknerr.de>
2019-07-05 11:03:18 +0200

    added HelloConsole app

---
+++ HelloConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Current time is: {DateTime.Now}");
+            Console.WriteLine("Hello dotnet!");
+        }
+    }
+}
---
+++ HelloConsole/README.md
@@ -0,0 +1,14 @@
+# HelloConsole
+
+Create new console app:
+```
+$ dotnet new console -o HelloConsole
+```
+
+Added `.gitignore` file :)
+
+Change `Program.cs` and run:
+```
+$ dotnet run
+```
+
